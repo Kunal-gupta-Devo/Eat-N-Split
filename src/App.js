55312e7b41e57db5851f3f56f2f9c90ed0a2ac67
KunalGupta <kunalgupta100320@gmail.com>
2025-04-28 00:57:58 +0530

    dispaly new friend in form

--- src/App.js
+++ src/App.js
@@ -22,16 +22,25 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriends() {
     setShowAddFriend((s) => !s);
   }
+
+  function handleAddFriend(friend) {
+    // we use spread here b/c it not mutate original array as like push
+    setFriends((friends) => [...friends, friend]);
+
+    // b/c we need to hide form after sumbit
+    showAddFriend(false);
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriends}>
           {showAddFriend === true ? "close" : "Add Friends"}
         </Button>
@@ -48,8 +57,8 @@ function Button({ children, onClick }) {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
+function FriendsList({ friends }) {
+  // const friends = initialFriends;
   return (
     <ul>
       {friends.map((friend) => (
@@ -87,13 +96,44 @@ function Friend({ friend }) {
 
 // Add Friends Form
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+  const id = crypto.randomUUID();
+  function handleSubmit(e) {
+    e.preventDefault();
+    // onsubmit always need prvent defult method
+
+    if (!name || !image) return;
+
+    // here we had created a object that take all data form current state change
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    console.log(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+
+    onAddFriend(newFriend);
+  }
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👯 Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <lebel>📷Image URL</lebel>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
